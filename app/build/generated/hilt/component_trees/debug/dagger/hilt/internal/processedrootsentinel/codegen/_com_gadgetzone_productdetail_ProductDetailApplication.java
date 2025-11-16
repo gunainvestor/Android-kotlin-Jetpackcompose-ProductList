dd2433bbd273eb13585f9c5ed4a7bcbe002eba7e
Guna <gadgetzone@Gunas-MacBook-Pro.local>
2025-11-16 16:02:15 +0530

    Remove all remaining build files from git tracking

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_gadgetzone_productdetail_ProductDetailApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.gadgetzone.productdetail.ProductDetailApplication"
-)
-public final class _com_gadgetzone_productdetail_ProductDetailApplication {
-}
